248f85503279b87b9f1b5333babcf3bb56f5c0df
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-06-27 17:13:06 +0800

    https -> http

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -53,11 +53,11 @@
         methods: {
             linkClick() {
                 axios
-                .patch(`https://articles-recommender.azurewebsites.net/api/clicks/${this.id}`)
+                .patch(`http://articles-recommender.azurewebsites.net/api/clicks/${this.id}`)
             },
             love() {
                 axios
-                .patch(`https://articles-recommender.azurewebsites.net/api/loves/${this.id}`)
+                .patch(`http://articles-recommender.azurewebsites.net/api/loves/${this.id}`)
                 .then(response => this.loveCount = response['data']['loves'])
             }
         },
