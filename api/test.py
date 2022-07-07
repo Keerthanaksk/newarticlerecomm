29ae73f42fce33d2919c9859a44b88d25480e625
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-07-07 21:44:09 +0800

    env test

--- api/test.py
+++ api/test.py
@@ -1,10 +1,18 @@
-# import os
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = FastAPI()
 
 @app.get('/')
 async def root():
-    # x = os.environ.get('TEST')
-    return {'msg': 'hey'}
+    # azure app
+    x = os.environ.get('TEST')
+    return {'msg': x}
+
+@app.get('/test')
+async def root():
+    # .env
+    x = os.environ.get('MONGO_DB')
+    return {'msg': x}
