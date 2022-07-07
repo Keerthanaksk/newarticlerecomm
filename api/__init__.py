59bbe60e50beeb71026ffbcb520aa77495e16e7f
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-07-07 21:34:36 +0800

    comment init

--- api/__init__.py
+++ api/__init__.py
@@ -1,58 +1,58 @@
-import os
+# import os
 
-from api.core.config import settings
-from api.db.mongodb_utils import connect_to_mongo, close_mongo_connection
-from api.api.v1.api import api_router
+# from api.core.config import settings
+# from api.db.mongodb_utils import connect_to_mongo, close_mongo_connection
+# from api.api.v1.api import api_router
 
-from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+# from starlette.middleware.cors import CORSMiddleware
+# from fastapi import FastAPI
 
-from fastapi_jwt_auth import AuthJWT
+# from fastapi_jwt_auth import AuthJWT
 
 
 
-def register_cors(app: FastAPI):
+# def register_cors(app: FastAPI):
     
-    if os.getenv('FASTAPI_ENV') == 'prod':
-        origins = [
-            os.getenv('FRONTEND_ORIGIN')
-        ]
-    else:
-        origins = [
-            'http://localhost:8080'
-        ]
+#     if os.getenv('FASTAPI_ENV') == 'prod':
+#         origins = [
+#             os.getenv('FRONTEND_ORIGIN')
+#         ]
+#     else:
+#         origins = [
+#             'http://localhost:8080'
+#         ]
     
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=origins,
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
 
 
 
-def register_fastapi_jwt_auth(app: FastAPI):
-    @AuthJWT.load_config
-    def get_config():
-        return settings
+# def register_fastapi_jwt_auth(app: FastAPI):
+#     @AuthJWT.load_config
+#     def get_config():
+#         return settings
 
 
 
-def create_app():
-    app = FastAPI(
-        title=settings.PROJECT_NAME, 
-        description=settings.PROJECT_DESC,
-        version=settings.PROJECT_VERSION
-    )
+# def create_app():
+#     app = FastAPI(
+#         title=settings.PROJECT_NAME, 
+#         description=settings.PROJECT_DESC,
+#         version=settings.PROJECT_VERSION
+#     )
 
-    app.include_router(api_router)
+#     app.include_router(api_router)
 
-    app.add_event_handler("startup", connect_to_mongo)
-    app.add_event_handler("shutdown", close_mongo_connection)
+#     app.add_event_handler("startup", connect_to_mongo)
+#     app.add_event_handler("shutdown", close_mongo_connection)
 
-    register_cors(app)
-    register_fastapi_jwt_auth(app)
+#     register_cors(app)
+#     register_fastapi_jwt_auth(app)
 
-    return app
+#     return app
 
