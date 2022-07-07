f87db8605c01cddd03292e853f84f9ef9e18e33f
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-07-07 23:56:11 +0800

    more test

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ export default createStore({
         return {
             username: cookie.get('username'),
             authenticated: cookie.get('authenticated') === 'true',
-            API_BASE_URL: process.env.VUE_APP_ENV == 'prod' ? process.env.VUE_APP_API_BASE_URL : 'http://localhost:8000/'
+            API_BASE_URL: process.env.VUE_APP_ENV == 'prod' ? process.env.VUE_APP_API_BASE_URL : 'http://localhost:8000/',
+            test: process.env.VUE_APP_ENV
         }
     },
     mutations: {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,6 +5,7 @@
                 <div class="card border-0 shadow mt-5">
                     <div class="card-body p-5">
                         {{this.$store.state.API_BASE_URL}}
+                        {{this.$store.state.test}}
                         <h1 class="card-title display-6 text-center fw-bolder">Welcome Back!</h1>
                         <h6 class="card-subtitle mb-4 text-muted text-center">Intelligent Newsletter by UnionBank and Aboitiz</h6>
                         
