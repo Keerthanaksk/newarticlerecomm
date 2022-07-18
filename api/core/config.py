5d2e9c547edd9fde831884c67da419177a813745
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-07-18 18:19:23 +0800

    config cookie samesite

--- api/core/config.py
+++ api/core/config.py
@@ -22,6 +22,8 @@ class Settings(BaseSettings):
     # Disable CSRF Protection for this example. default is True
     AUTHJWT_COOKIE_CSRF_PROTECT: bool = False
 
+    authjwt_cookie_samesite = None
+
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 60 # 1 hour
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 # 1 week
 
