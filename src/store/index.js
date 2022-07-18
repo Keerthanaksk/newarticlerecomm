accd0475e6f93cd7269465c277d18e744d17a9cb
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-07-18 14:31:39 +0800

    added proxy server

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ export default createStore({
     state () {
         return {
             email: cookie.get('email'),
-            API_BASE_URL: process.env.VUE_APP_ENV == 'prod' ? process.env.VUE_APP_API_BASE_URL : 'http://localhost:8000/'
+            API_BASE_URL: 'http://localhost:8080/'
+            // API_BASE_URL: process.env.VUE_APP_ENV == 'prod' ? process.env.VUE_APP_API_BASE_URL : 'http://localhost:8080/'
         }
     },
     mutations: {
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    devServer: {
+      proxy: 'https://articles-recommender.azurewebsites.net/'
+    }
+  }
+  
